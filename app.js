780b512b1c814d3136b9a05b02bef895358f5ee3
francesco <fgiananti@gmail.com>
2018-05-07 07:38:26 +0200

    Add connect and disconnect event handlers

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ require('./config/config');
 const express = require('express'),
       app = express(),
       path = require('path'),
-      port = process.env.PORT;
+      port = process.env.PORT,
+      socketIO = require('socket.io'),
+      // bisogna creare un server utilizzando la library http. di fatto express utilizza anch'esso la library... quando si fa app.listen, behind the scenes viene chiamato http.createServer
+      http = require('http'),
+      server = http.createServer(app),
+      // io = websockect server
+      io = socketIO(server);
+
+
 
 const indexRoutes = require('./routes/index');
 
@@ -13,6 +21,15 @@ app.set('view engine', 'ejs');
 
 app.use('/', indexRoutes);
 
-app.listen(process.env.PORT, () => {
+io.on('connection', (socket) => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user was disconnected')
+  })
+});
+
+// socket.io richiede server qui
+server.listen(process.env.PORT, () => {
   console.log('connected to the server');
 });
