81aba9d1cf564e09c27469f639458421cdb29833
francesco <fgiananti@gmail.com>
2018-05-07 14:31:56 +0200

    Greet new user, and alert others

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const express = require('express'),
       path = require('path'),
       port = process.env.PORT,
       socketIO = require('socket.io'),
-      // bisogna creare un server utilizzando la library http. di fatto express utilizza anch'esso la library... quando si fa app.listen, behind the scenes viene chiamato http.createServer
       http = require('http'),
       server = http.createServer(app),
-      // io = websockect server
       io = socketIO(server),
-      // usato path per conoscenza (è nuts)
       publicPath = path.join(__dirname, './public');
 
 const indexRoutes = require('./routes/index');
@@ -22,23 +19,40 @@ app.use('/', indexRoutes);
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  // tradotto: server in "ascolto" dell'evento 'createMessage' dal client
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('New message', message);
-    //
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
     console.log('user was disconnected');
-  })
+  });
 });
 
-// socket.io richiede server qui
+
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,22 @@
 let socket = io();
 // CLIENT MESSAGES
 
-// tradotto: client in "ascolto" dell'evento 'connect' dal server
+// in "ascolto" dell'evento disconnect dal server
 socket.on('connect', function() {
   console.log('Connected to server');
-
 });
-// tradotto: client in "ascolto" dell'evento 'disconnect' dal server
+
+// in "ascolto" dell'evento disconnect dal server
 socket.on('disconnect', function() {
   console.log('Disconnected from server')
 });
 
-// tradotto: client in "ascolto" dell'evento 'newEmail' dal server
+// in "ascolto" dell'evento newUser dal server
+socket.on('newUser', function (message) {
+  console.log(message);
+});
+
+// in "ascolto" dell'evento newMessage dal server
 socket.on('newMessage', function (message) {
   console.log('New message', message);
 });
